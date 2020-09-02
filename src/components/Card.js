de9605b83cd125c72d0ad19216448bd0ffa338ff
Enyel Sequeira <enyelsequeira@hotmail.com>
2020-09-02 11:48:34 +0100

    trying something new faster

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,21 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
-const Card = ({ title, image, likeCount, onCardImageClick }) => {
-  console.log(title);
+const Card = ({ title, image, likeCount, onCardImageClick }) => (
+  // console.log(title);
 
-  return (
-    <li className="elements__item">
-      <div
-        className="elements__image"
-        onClick={onCardImageClick}
-        style={{ backgroundImage: `url(${image})` }}
-      />
-      <button className="elements__trash" type="button" />
-      <div className="elements__info">
-        <h3 className="elements__title">{title}</h3>
-        <button className="elements__image-heart" type="button" name="like" />
-        <h4 className="elements__like-count">{likeCount}</h4>
-      </div>
-    </li>
-  );
-};
-
+  <li className="elements__item">
+    <div
+      className="elements__image"
+      onClick={onCardImageClick}
+      style={{ backgroundImage: `url(${image})` }}
+    />
+    <button className="elements__trash" type="button" />
+    <div className="elements__info">
+      <h3 className="elements__title">{title}</h3>
+      <button className="elements__image-heart" type="button" name="like" />
+      <h4 className="elements__like-count">{likeCount}</h4>
+    </div>
+  </li>
+);
 export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,47 +3,37 @@ import api from '../utils/Api.js';
 import Card from './Card.js';
 
 const Main = ({ onCardImageClick, onAddCard, onEditAvatar, onEditProfile }) => {
-  // console.log(props, 22222);
-  const [name, setName] = useState('');
-  const [about, setAbout] = useState('');
-  const [avatar, setAvatar] = useState('');
+  const [userData, setUserData] = useState({});
   const [cards, setCards] = useState([]);
 
-  // useEffect(() => {
-  //   api.getUserInfo().then((res) => {
-  //     setAvatar(res.avatar);
-  //     setName(res.name);
-  //     setAbout(res.about);
-  //   });
-  // }, [name, about, avatar]);
   useEffect(() => {
-    api.getUserInfo().then((res) => {
-      setAvatar(res.avatar);
-      setName(res.name);
-      setAbout(res.about);
-    });
-    return () => {
-      api.getCardList().then((res) => {
-        setCards(res);
-      });
+    const fetchData = async () => {
+      const [fetchedData, fetchedCards] = await Promise.all([
+        api.getUserInfo(),
+        api.getCardList(),
+      ]);
+
+      setUserData(fetchedData);
+      setCards(fetchedCards);
     };
-  }, [setCards, name, about, avatar]);
-  // useEffect(() => {
-  //   api.getCardList().then((res) => {
-  //     setCards(res);
-  //   });
-  // }, [setCards]);
+
+    fetchData();
+  }, []);
 
   return (
     <main>
       <section className="profile">
         <div className="profile__avatar">
-          <img className="profile__avatar-image" src={avatar} alt="profile" />
+          <img
+            className="profile__avatar-image"
+            src={userData.avatar}
+            alt="profile"
+          />
           <button className="profile__avatar-overlay" onClick={onEditAvatar} />
         </div>
         <div className="profile__info">
-          <h2 className="profile__name">{name}</h2>
-          <p className="profile__profession">{about}</p>
+          <h2 className="profile__name">{userData.name}</h2>
+          <p className="profile__profession">{userData.about}</p>
           <button className="profile__info-btn" onClick={onEditProfile} />
         </div>
         <button className="profile__button-add" onClick={onAddCard} />
